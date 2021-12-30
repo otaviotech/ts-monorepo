238aba3204c31da7fe25bb3880c918fc0918c771
Otavio Araujo <otaviotech@gmail.com>
2021-12-30 18:34:11 -0300

    test(server): add SignUp usecase
    -

--- packages/server/src/presentation/controllers/signup.test.ts
+++ packages/server/src/presentation/controllers/signup.test.ts
@@ -3,11 +3,35 @@ import { RequestValidationError } from '../errors/validation';
 import { omit } from '../../../test/utils';
 import { SignUpInputValidator } from '../validators/signUp';
 import { HttpRequest } from '../protocols/http';
+import { SignUp, SignUpUseCaseInput } from '../../domain/usecases/signup';
+import { Profile } from '../../domain/models/profile';
 
 describe('RegisterController', () => {
+  const makeSignUpUseCaseStub = () => {
+    class SignUpUseCaseStub implements SignUp {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      async signup(input: SignUpUseCaseInput): Promise<Profile> {
+        return {
+          id: 1,
+          userId: 1,
+          username: 'jdoe',
+          name: 'John Doe',
+          site: '',
+          bio: '',
+          email: 'johndoe@email.com',
+          phone: '',
+        };
+      }
+    }
+
+    return new SignUpUseCaseStub();
+  };
   const makeSut = () => {
     const signUpInputValidator = new SignUpInputValidator();
-    const sut = new SignUpController(signUpInputValidator);
+    const signUpUseCaseStub = makeSignUpUseCaseStub();
+
+    const sut = new SignUpController(signUpInputValidator, signUpUseCaseStub);
+
     const validRequest: HttpRequest = {
       body: {
         email: 'johndoe@email.com',
@@ -19,6 +43,7 @@ describe('RegisterController', () => {
     return {
       sut,
       validRequest,
+      signUpUseCaseStub,
     };
   };
 
@@ -42,4 +67,17 @@ describe('RegisterController', () => {
       expect(httpResponse.data).toBeUndefined();
     }
   );
+
+  it('should call the SignUp usecase if validation passes', async () => {
+    const { sut, signUpUseCaseStub, validRequest } = makeSut();
+    jest.spyOn(signUpUseCaseStub, 'signup');
+
+    const httpResponse = await sut.handle(validRequest);
+
+    expect(signUpUseCaseStub.signup).toHaveBeenCalledWith({
+      ...(validRequest.body as SignUpUseCaseInput),
+    });
+
+    expect(httpResponse.statusCode).toBe(201);
+  });
 });
