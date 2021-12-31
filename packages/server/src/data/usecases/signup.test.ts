b03ee78d7728d5e24c2dc166493d7a4885cab538
Otavio Araujo <otaviotech@gmail.com>
2021-12-31 15:13:09 -0300

    test(server): validate if user can be created

--- packages/server/src/data/usecases/signup.test.ts
+++ packages/server/src/data/usecases/signup.test.ts
@@ -1,6 +1,14 @@
+// eslint-disable-next-line max-classes-per-file
+import { User } from '../../domain/models/user';
 import { SignUpUseCaseInput } from '../../domain/usecases/signup';
+import { FindUserByEmailRepository } from '../protocols/findUserByEmailRepository';
 import { PasswordHasher } from '../protocols/passwordHasher';
 import { SignUpUseCase } from './signup';
+import { EmailAreadyTakenError } from '../../domain/errors/emailAlreadyTaken';
+import { FindProfileByEmailRepository } from '../protocols/findProfileByEmailRepository';
+import { Profile } from '../../domain/models/profile';
+import { UsernameAlreadyTakenError } from '../../domain/errors/usernameAlreadyTaken';
+import { FindProfileByUsernameRepository } from '../protocols/findProfileByEmailRepository copy';
 
 const makePasswordHasherStub = () => {
   class PasswordHasherStub implements PasswordHasher {
@@ -12,8 +20,51 @@ const makePasswordHasherStub = () => {
   return new PasswordHasherStub();
 };
 
+const makeFindUserByEmailRepoStub = () => {
+  class FindUserByEmailRepoStub implements FindUserByEmailRepository {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async find(email: string): Promise<User> {
+      return {
+        id: 1,
+        email: 'johndoe@email.com',
+        name: 'John Doe',
+        password: 'abc123',
+      };
+    }
+  }
+
+  return new FindUserByEmailRepoStub();
+};
+
+const makeFindProfileByEmailStub = () => {
+  class FindProfileByEmailStub implements FindProfileByEmailRepository {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async find(email: string): Promise<Profile> {
+      return {} as Profile;
+    }
+  }
+
+  return new FindProfileByEmailStub();
+};
+
+const makeFindProfileByUsernameRepo = () => {
+  class FindProfileByUsernameRepositoryStub
+    implements FindProfileByUsernameRepository
+  {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async find(username: string): Promise<Profile> {
+      return {} as Profile;
+    }
+  }
+
+  return new FindProfileByUsernameRepositoryStub();
+};
+
 const makeSut = () => {
   const passwordHasherStub = makePasswordHasherStub();
+  const findUserByEmailRepoStub = makeFindUserByEmailRepoStub();
+  const findProfileByEmailRepoStub = makeFindProfileByEmailStub();
+  const findProfileByUsernameRepoStub = makeFindProfileByUsernameRepo();
 
   const validInput = {
     email: 'johndoe@email.com',
@@ -21,18 +72,45 @@ const makeSut = () => {
     password: 'abc123',
   } as SignUpUseCaseInput;
 
-  const sut = new SignUpUseCase(passwordHasherStub);
+  const sut = new SignUpUseCase(
+    passwordHasherStub,
+    findUserByEmailRepoStub,
+    findProfileByEmailRepoStub,
+    findProfileByUsernameRepoStub
+  );
 
   return {
     sut,
     passwordHasherStub,
+    findUserByEmailRepoStub,
+    findProfileByEmailRepoStub,
+    findProfileByUsernameRepoStub,
     validInput,
   };
 };
 
 describe('SignUpUseCase', () => {
   it('should hash the password', async () => {
-    const { sut, passwordHasherStub, validInput } = makeSut();
+    const {
+      sut,
+      passwordHasherStub,
+      findUserByEmailRepoStub,
+      findProfileByEmailRepoStub,
+      findProfileByUsernameRepoStub,
+      validInput,
+    } = makeSut();
+
+    jest
+      .spyOn(findUserByEmailRepoStub, 'find')
+      .mockResolvedValueOnce(undefined);
+
+    jest
+      .spyOn(findProfileByEmailRepoStub, 'find')
+      .mockResolvedValueOnce(undefined);
+
+    jest
+      .spyOn(findProfileByUsernameRepoStub, 'find')
+      .mockResolvedValueOnce(undefined);
 
     jest.spyOn(passwordHasherStub, 'hash');
 
@@ -42,7 +120,26 @@ describe('SignUpUseCase', () => {
   });
 
   it('should throw if the password hasher throws', async () => {
-    const { sut, passwordHasherStub, validInput } = makeSut();
+    const {
+      sut,
+      passwordHasherStub,
+      findUserByEmailRepoStub,
+      findProfileByEmailRepoStub,
+      findProfileByUsernameRepoStub,
+      validInput,
+    } = makeSut();
+
+    jest
+      .spyOn(findUserByEmailRepoStub, 'find')
+      .mockResolvedValueOnce(undefined);
+
+    jest
+      .spyOn(findProfileByEmailRepoStub, 'find')
+      .mockResolvedValueOnce(undefined);
+
+    jest
+      .spyOn(findProfileByUsernameRepoStub, 'find')
+      .mockResolvedValueOnce(undefined);
 
     jest
       .spyOn(passwordHasherStub, 'hash')
@@ -51,5 +148,55 @@ describe('SignUpUseCase', () => {
     const promise = sut.signup(validInput);
 
     expect(promise).rejects.toThrow(new Error('ERROR'));
+  });
+
+  it("should throw if there's already a user with the same email", () => {
+    const { sut, validInput } = makeSut();
+
+    const promise = sut.signup(validInput);
+
+    expect(promise).rejects.toThrow(new EmailAreadyTakenError());
+  });
+
+  it("should throw if there's already a profile with the same email", () => {
+    const {
+      sut,
+      findUserByEmailRepoStub,
+      findProfileByEmailRepoStub,
+      validInput,
+    } = makeSut();
+
+    jest
+      .spyOn(findUserByEmailRepoStub, 'find')
+      .mockResolvedValueOnce(undefined);
+
+    jest
+      .spyOn(findProfileByEmailRepoStub, 'find')
+      .mockResolvedValueOnce(undefined);
+
+    const promise = sut.signup(validInput);
+
+    expect(promise).rejects.toThrow(new EmailAreadyTakenError());
+  });
+
+  it("should throw if there's already a profile with the same username", () => {
+    const {
+      sut,
+      findUserByEmailRepoStub,
+      findProfileByEmailRepoStub,
+      validInput,
+    } = makeSut();
+
+    jest
+      .spyOn(findUserByEmailRepoStub, 'find')
+      .mockResolvedValueOnce(undefined);
+
+    jest
+      .spyOn(findProfileByEmailRepoStub, 'find')
+      .mockResolvedValueOnce(undefined);
+
+    const promise = sut.signup(validInput);
+
+    expect(promise).rejects.toThrow(new UsernameAlreadyTakenError());
   });
 });
