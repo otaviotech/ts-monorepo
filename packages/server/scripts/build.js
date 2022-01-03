29eba66076d518105472c6bf2052631db8e41e67
Otávio Araújo <otaviotech@gmail.com>
2022-01-03 20:34:55 -0300

    build(server): improve build routine

---
+++ packages/server/scripts/build.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const shell = require('shelljs');
+const path = require('path');
+const tsConfig = require('../tsconfig.json');
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+async function cleanOutDir() {
+  console.log('Cleaning outDir...');
+
+  const { outDir } = tsConfig.compilerOptions;
+  const outDirPath = path.resolve(__dirname, '../', outDir);
+
+  shell.rm('-r', outDirPath);
+}
+
+async function compileTS() {
+  console.log('Compiling TypeScript...');
+  shell.exec('npm run build:ts');
+}
+
+async function copyEnvFile() {
+  console.log('Copying .env file...');
+
+  const source = path.resolve(__dirname, '../.env');
+  const dest = path.resolve(__dirname, '../build/.env');
+
+  shell.cp(source, dest);
+}
+
+async function build() {
+  await cleanOutDir();
+  await compileTS();
+
+  if (isDev) {
+    await copyEnvFile();
+  }
+}
+
+build()
+  .then(() => {
+    console.log('Finished!');
+    process.exit(0);
+  })
+  .catch(console.error);
