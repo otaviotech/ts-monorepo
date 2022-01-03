6c02b0393c62cfae51a120850867d6b529e7bb8d
Otávio Araújo <otaviotech@gmail.com>
2022-01-03 11:47:57 -0300

    test(server): implement PrismaFindProfileByUsernameRepository

---
+++ packages/server/src/infra/db/postgres/prisma/repositories/user/findProfileByUsername.test.ts
@@ -0,0 +1,41 @@
+import { Profile as PrismaProfile } from '@prisma/client';
+import { createMockContext } from '@test/mocks/prismaContext';
+import { PrismaFindProfileByUsername } from './findProfileByUsername';
+
+describe('PrismaProfileByUsernameRepository', () => {
+  const makeSut = () => {
+    const prismaContext = createMockContext();
+    const sut = new PrismaFindProfileByUsername(prismaContext.prisma);
+    const validInput = 'johndoe@email.com';
+
+    return {
+      sut,
+      prismaContext,
+      validInput,
+    };
+  };
+
+  it('should call Prisma with the correct payload', async () => {
+    const { sut, prismaContext, validInput } = makeSut();
+
+    jest
+      .spyOn(prismaContext.prisma.profile, 'findFirst')
+      .mockResolvedValueOnce({ id: 1 } as PrismaProfile);
+
+    const result = await sut.find(validInput);
+
+    expect(result?.id).toBe(1);
+  });
+
+  it('should return undefined profile is not found', async () => {
+    const { sut, prismaContext, validInput } = makeSut();
+
+    jest
+      .spyOn(prismaContext.prisma.profile, 'findFirst')
+      .mockResolvedValueOnce(null);
+
+    const result = await sut.find(validInput);
+
+    expect(result).toBeUndefined();
+  });
+});
