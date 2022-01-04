f650ddbb94991f01d97bbd5ec27c71a6678e79b0
Otávio Araújo <otaviotech@gmail.com>
2022-01-04 16:37:43 -0300

    docs(docs): setup brand colors and editUrl
    -

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/otaviotech/ts-monorepo/tree/main/packages/docs/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
