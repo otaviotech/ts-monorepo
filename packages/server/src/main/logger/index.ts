6ee24c89d3a2daf556631a535607f0d5f3f5a54f
Otávio Araújo <otaviotech@gmail.com>
2022-01-07 15:00:40 -0300

    feat(server): implement http request logging

---
+++ packages/server/src/main/logger/index.ts
@@ -0,0 +1,3 @@
+import morgan from 'morgan';
+
+export const httpRequestLogger = morgan('combined');
--- packages/server/src/main/server/middlewares/index.ts
+++ packages/server/src/main/server/middlewares/index.ts
@@ -2,8 +2,10 @@ import { Express } from 'express';
 
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
+import { httpRequestLogger } from './logger';
 
 export const registerMiddlewares = (app: Express) => {
+  app.use(httpRequestLogger);
   app.use(bodyParser.json());
   app.use(helmet());
 };
