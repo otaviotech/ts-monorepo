646baf1df67b71469b80d247468731c3745f7acb
Otávio Araújo <otaviotech@gmail.com>
2022-01-07 10:14:00 -0300

    test(server): implement JwtTokenGenerator

---
+++ packages/server/src/infra/authTokenGenerator/jwtTokenGenerator.test.ts
@@ -0,0 +1,53 @@
+import jwt from 'jsonwebtoken';
+import { JwtTokenGenerator } from './jwtTokenGenerator';
+
+describe('JwtTokenGenerator', () => {
+  const makeSut = () => {
+    const sut = new JwtTokenGenerator('SECRET_KEY');
+    const validInput = { id: 1 };
+
+    return { sut, validInput };
+  };
+
+  it('should generate a jwt token using the secret and the payload', async () => {
+    const { sut, validInput } = makeSut();
+
+    const JWT_RESULT = 'A.JWT.TOKEN';
+
+    jest
+      .spyOn(jwt, 'sign')
+      .mockImplementationOnce((a, b, c, cb) => cb(null, JWT_RESULT));
+
+    const result = await sut.generate(validInput);
+
+    expect(result).toBe(JWT_RESULT);
+  });
+
+  it('should throw if jwt throws', async () => {
+    const { sut, validInput } = makeSut();
+
+    const errorThrown = new Error('Error throw by jsonwebtoken');
+
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw errorThrown;
+    });
+
+    const promise = sut.generate(validInput);
+
+    expect(promise).rejects.toThrow(errorThrown);
+  });
+
+  it('should throw if jwt has an error', async () => {
+    const { sut, validInput } = makeSut();
+
+    const errorThrown = new Error('Error throw by jsonwebtoken');
+
+    jest
+      .spyOn(jwt, 'sign')
+      .mockImplementationOnce((a, b, c, cb) => cb(errorThrown, undefined));
+
+    const promise = sut.generate(validInput);
+
+    expect(promise).rejects.toThrow(errorThrown);
+  });
+});
